fb36e6e90b66c9df73cf895681c0e838be173728
Cody Fidler <c.fidler@chargebacks911.com>
2017-11-07 00:21:27 -0500

    Update farbtastic.js

--- src/farbtastic.js
+++ src/farbtastic.js
@@ -166,7 +166,7 @@ $._farbtastic = function (container, options) {
           // New color
           color2 = fb.pack(fb.HSLToRGB([d2, 1, 0.5]));
       if (i > 0) {
-        if ($.browser.msie || false) {
+        if ( (typeof G_vmlCanvasManager != 'undefined' && $.browser.msie) || false) {
           // IE's gradient calculations mess up the colors. Correct along the diagonals.
           var corr = (1 + Math.min(Math.abs(Math.tan(angle1)), Math.abs(Math.tan(Math.PI / 2 - angle1)))) / n;
           color1 = fb.pack(fb.HSLToRGB([d1 - 0.15 * corr, 1, 0.5]));
@@ -246,7 +246,7 @@ $._farbtastic = function (container, options) {
       fb.ctxMask.drawImage(buffer, 0, 0, sz + 1, sz + 1, -sq, -sq, sq * 2, sq * 2);
     }
     // Method #2: drawing commands (old Canvas).
-    else if (!($.browser.msie || false)) {
+    else if (!((typeof G_vmlCanvasManager != 'undefined' && $.browser.msie) || false)) {
       // Render directly at half-resolution
       var sz = Math.floor(size / 2);
       calculateMask(sz, sz, function (x, y, c, a) {
